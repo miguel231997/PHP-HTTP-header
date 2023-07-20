49f927ab1d3a4a9b9c845f9edc01aa963aa4d523
Miguel Galindo <miguelgalindo1214@gmail.com>
2023-07-20 12:21:20 -0400

    Loading env variables to project

--- index.php
+++ index.php
@@ -3,6 +3,10 @@
 require_once 'vendor/autload.php';
 
 use App\Controller\HomeController;
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
 $controller = new HomeController();
 $controller->index();
